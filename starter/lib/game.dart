4e38126ab2b725924aa25827e5c3a9be4b33808c
Shivang10799 <91725545+Shivang10799@users.noreply.github.com>
2021-10-02 23:51:11 +0530

    Add files via upload

---
+++ starter/lib/game.dart
@@ -0,0 +1,223 @@
+/* Copyright (c) 2020 Razeware LLC
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom
+the Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+Notwithstanding the foregoing, you may not use, copy, modify,
+merge, publish, distribute, sublicense, create a derivative work,
+and/or sell copies of the Software in any work that is designed,
+intended, or marketed for pedagogical or instructional purposes
+related to programming, coding, application development, or
+information technology. Permission for such use, copying,
+modification, merger, publication, distribution, sublicensing,
+creation of derivative works, or sale is expressly withheld.
+
+This project and source code may use libraries or frameworks
+that are released under various Open-Source licenses. Use of
+those libraries and frameworks are governed by their own
+individual licenses.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE. */
+
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'direction.dart';
+import 'piece.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<GamePage> {
+  List<Offset> positions = [];
+  int length = 5;
+  int step = 20;
+  Direction direction = Direction.right;
+
+  Piece food;
+  Offset foodPosition;
+
+  double screenWidth;
+  double screenHeight;
+  int lowerBoundX, upperBoundX, lowerBoundY, upperBoundY;
+
+  Timer timer;
+  double speed = 1;
+
+  int score = 0;
+
+  void draw() async {
+    // TODO
+  }
+
+  Direction getRandomDirection([String type]) {
+    if (type == "horizontal") {
+      bool random = Random().nextBool();
+      if (random) {
+        return Direction.right;
+      } else {
+        return Direction.left;
+      }
+    } else if (type == "vertical") {
+      bool random = Random().nextBool();
+      if (random) {
+        return Direction.up;
+      } else {
+        return Direction.down;
+      }
+    } else {
+      int random = Random().nextInt(4);
+      return Direction.values[random];
+    }
+  }
+
+  Offset getRandomPositionWithinRange() {
+    int posX = Random().nextInt(upperBoundX) + lowerBoundX;
+    int posY = Random().nextInt(upperBoundY) + lowerBoundY;
+    return Offset(roundToNearestTens(posX).toDouble(), roundToNearestTens(posY).toDouble());
+  }
+
+  bool detectCollision(Offset position) {
+    // TODO
+  }
+
+  void showGameOverDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (ctx) {
+        return AlertDialog(
+          backgroundColor: Colors.red,
+          shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: Colors.black,
+                width: 3.0,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text(
+            "Game Over",
+            style: TextStyle(color: Colors.white),
+          ),
+          content: Text(
+            "Your game is over but you played well. Your score is " + score.toString() + ".",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+                restart();
+              },
+              child: Text(
+                "Restart",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<Offset> getNextPosition(Offset position) async {
+    // TODO
+  }
+
+  void drawFood() {
+    // TODO
+  }
+
+  List<Piece> getPieces() {
+    // TODO
+  }
+
+  Widget getControls() {
+    // TODO
+  }
+
+  int roundToNearestTens(int num) {
+    int divisor = step;
+    int output = (num ~/ divisor) * divisor;
+    if (output == 0) {
+      output += step;
+    }
+    return output;
+  }
+
+  void changeSpeed() {
+    // TODO
+  }
+
+  Widget getScore() {
+    // TODO
+  }
+
+  void restart() {
+    // TODO
+  }
+
+  Widget getPlayAreaBorder() {
+    return Positioned(
+      top: lowerBoundY.toDouble(),
+      left: lowerBoundX.toDouble(),
+      child: Container(
+        width: (upperBoundX - lowerBoundX + step).toDouble(),
+        height: (upperBoundY - lowerBoundY + step).toDouble(),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black.withOpacity(0.2),
+            style: BorderStyle.solid,
+            width: 1.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    restart();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
+    lowerBoundX = step;
+    lowerBoundY = step;
+    upperBoundX = roundToNearestTens(screenWidth.toInt() - step);
+    upperBoundY = roundToNearestTens(screenHeight.toInt() - step);
+
+    return Scaffold(
+      body: Container(
+        color: Color(0XFFF5BB00),
+        child: Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
